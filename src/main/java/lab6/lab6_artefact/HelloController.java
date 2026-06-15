7560d4b08bf562acd3f7af46c38bb4fa707af09c
Vitor Emmanuel <vitoremmanuelprofissionalti@gmail.com>
2026-06-15 15:45:21 -0300

    Configurei o projeto para usar o apache tomcat, e o arquivo.war ficar com o nome de myapp

--- src/main/java/lab6/lab6_artefact/HelloController.java
+++ src/main/java/lab6/lab6_artefact/HelloController.java
@@ -2,12 +2,20 @@ package lab6.lab6_artefact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class HelloController {
+public class HelloController extends SpringBootServletInitializer {
+
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloController.class);
+    } 
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloController.class, args);
